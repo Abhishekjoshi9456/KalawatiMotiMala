637e671327f9e3b50ed0eb15e7ec5785d9a715c2
ahasdalitem <aniketlitem@gmail.com>
2025-06-03 10:15:39 +0530

    ui

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -72,7 +72,7 @@ class ProductController extends Controller
             }
 
             $product->save();
-
+            echo $product->product_id; die;
             if ($request->hasFile('pro_imageMulti')) {
                 foreach ($request->file('pro_imageMulti') as $image) {
                     $imageName = time() . '_' . uniqid() . '.' . $image->getClientOriginalExtension();
