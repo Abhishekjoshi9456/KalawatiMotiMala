2b82c39dbd7f26c7d31c73e0f6e2252b5ff26500
ahasdalitem <aniketlitem@gmail.com>
2025-06-05 15:55:47 +0530

    enquiry

--- resources/views/enquiry/enquiry-list.blade.php
+++ resources/views/enquiry/enquiry-list.blade.php
@@ -16,7 +16,70 @@
     <!-- Main Content Area -->
     <main class="main-content">
         <div class="container-fluid">
+            <form action="#" class="row mb-3">
+                <div class="col-md-5">
+                    <label for="search">Search</label>
+                    <input type="search" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="from">Platform</label>
+                    <select name="" id="" class="form-control form-select">
+                        <option value="Select_Platform">Select Platform</option>
+                        <option value="Facebook">Facebook</option>
+                        <option value="instagram">Instagram</option>
+                        <option value="website">website</option>
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <label for="from">From</label>
+                    <input type="datetime" class="form-control">
+                </div>
+                <div class="col-md-2">
+                    <label for="from">To</label>
+                    <input type="datetime" class="form-control">
+                </div>
 
+            </form>
+            <div class="table-responsive scrollbar">
+                <table class="table table-bordered bg-light">
+                    <thead>
+                        <tr>
+                            <th colspan="6">Enquiry List</th>
+                        </tr>
+
+                        <tr>
+                            <th>S.NO.</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Platform</th>
+                            <th>Name</th>
+                            <th scope="col">Number</th>
+                            <th>Email</th>
+                            <th scope="col">Location</th>
+                            <th scope="col">Messages</th>
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>22-12-2024</td>
+                            <td>10:00PM</td>
+                            <td>Facebook</td>
+                            <td>Neeraj Bora</td>
+                            <td>neerajbora944@gmail.com</td>
+                            <td>+91 9027339904</td>
+                            <td>
+                                Delhi
+                            </td>
+                            <td>
+                                testing description
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     </main>
     @include ('include.footer')
@@ -26,4 +89,3 @@
 </body>
 
 </html>
-
