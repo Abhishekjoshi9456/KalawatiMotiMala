1bc8704a8f6e59afa8108d31c402dff1e358913c
ahasdalitem <aniketlitem@gmail.com>
2025-06-06 16:56:47 +0530

    enquery

--- app/Http/Controllers/EnquiryController.php
+++ app/Http/Controllers/EnquiryController.php
@@ -3,23 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EnquiryController extends Controller
 {
-    public function enquiryList(){
+    public function enquiryList()
+    {
         return view('enquiry.enquiry-list');
     }
 
-    public function SendEnquery(Request $request){
-        // $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'email' => 'required|email|max:255',
-        //     'phone' => 'required|digits_between:10,15',
-        //     'message' => 'required|string|max:500',
-        // ]);
-
-        // Here you would typically save the enquiry to the database
-        // For now, we will just return a success message
+    public function SendEnquery(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'number' => 'required|digits_between:10,15',
+            'location' => 'required|string|max:255',
+            'pincode' => 'required|digits:6',
+            'quentity' => 'required|integer|min:1',
+            'message' => 'required|string|max:1000',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()]);
+        }
 
         return  'Enquiry sent successfully!';
     }
